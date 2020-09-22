6a9a4ae46d6f84c9cbd4ec9e2e1b9b1810ef180a
Ben Ruwel <benruwel21@gmail.com>
2020-09-22 12:26:51 +0300

    Create utility class for Firebase Messaging service

---
+++ app/src/main/java/com/ruwel/bongachat/utils/MyFirebaseMessagingService.java
@@ -0,0 +1,22 @@
+package com.ruwel.bongachat.utils;
+
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+    }
+}
