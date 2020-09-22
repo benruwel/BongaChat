56763b32abaad1a7a38f7f8ed27d8f9ff8e754e3
Ben Ruwel <benruwel21@gmail.com>
2020-09-22 16:26:33 +0300

    Add logic to initialize FCM and get user's token when they login in

--- app/src/main/java/com/ruwel/bongachat/ui/LoginActivity.java
+++ app/src/main/java/com/ruwel/bongachat/ui/LoginActivity.java
@@ -1,15 +1,60 @@
 package com.ruwel.bongachat.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 import com.ruwel.bongachat.R;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //initialize FCM to get the user's token
+        initFCM();
     }
+
+    private void initFCM() {
+        // [START retrieve_current_token]
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+                        //send token to server
+                        sendRegistrationToServer(token);
+                    }
+                });
+        // [END retrieve_current_token]
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+        Log.d(TAG, "sendRegistrationToServer: sending token to server : " + refreshedToken);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        reference.child(getString(R.string.dbnode_users))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(getString(R.string.field_messaging_token))
+                .setValue(refreshedToken);
+    }
+    
 }
