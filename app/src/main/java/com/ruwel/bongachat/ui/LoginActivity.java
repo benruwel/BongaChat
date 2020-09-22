b9e43fe15b91b818ad5e0ccf3714a063a100d6e3
Ben Ruwel <benruwel21@gmail.com>
2020-09-22 16:21:49 +0300

    Generate signup and login activities and their layout files

---
+++ app/src/main/java/com/ruwel/bongachat/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.ruwel.bongachat.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.ruwel.bongachat.R;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
