f8359176027587b48295a01391da91e66a7dd0f1
Ben Ruwel <benruwel21@gmail.com>
2020-09-23 19:49:07 +0300

    Generate POJO for fcm data object

---
+++ app/src/main/java/com/ruwel/bongachat/models/FCMData.java
@@ -0,0 +1,39 @@
+package com.ruwel.bongachat.models;
+
+public class FCMData {
+    private String title;
+    private String message;
+    private String data_type;
+
+    public FCMData(String title, String message, String data_type) {
+        this.title = title;
+        this.message = message;
+        this.data_type = data_type;
+    }
+    public FCMData() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getData_type() {
+        return data_type;
+    }
+
+    public void setData_type(String data_type) {
+        this.data_type = data_type;
+    }
+}
