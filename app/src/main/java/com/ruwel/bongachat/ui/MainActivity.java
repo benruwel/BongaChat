631f6b9cd52feac900df720aad96bf486bb3aed4
Ben Ruwel <benruwel21@gmail.com>
2020-09-23 17:11:24 +0300

    Check the security level of a user to allow admin privileges

--- app/src/main/java/com/ruwel/bongachat/ui/MainActivity.java
+++ app/src/main/java/com/ruwel/bongachat/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ruwel.bongachat.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,6 +11,12 @@ import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ruwel.bongachat.R;
 
 import butterknife.BindView;
@@ -19,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.helloWorld)
     TextView textView;
+
+    private int mSecurity_level;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +57,35 @@ public class MainActivity extends AppCompatActivity {
             finish();
             startActivity(intent);
         }
+        if(id == R.id.admin) {
+            if(isAdmin()) {
+
+            } else {
+                Snackbar.make(textView, "You don't have admin privilege", Snackbar.LENGTH_SHORT)
+                        .setBackgroundTint(getResources().getColor(R.color.gray_dark))
+                        .setActionTextColor(getResources().getColor(R.color.gray))
+                        .show();
+            }
+        }
         return true;
+    }
+
+    private boolean isAdmin() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(getString(R.string.field_security_level)).orderByValue();
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                DataSnapshot singleSnapshot = snapshot.getChildren().iterator().next();
+                mSecurity_level = Integer.parseInt(singleSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return mSecurity_level == 10;
     }
 }
