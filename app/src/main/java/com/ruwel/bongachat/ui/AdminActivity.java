aeac72b307dea1d9a069be074c9ccaa82229f7d3
Ben Ruwel <benruwel21@gmail.com>
2020-09-23 17:16:52 +0300

    Generate Admin activity to allow for cloud messaging

---
+++ app/src/main/java/com/ruwel/bongachat/ui/AdminActivity.java
@@ -0,0 +1,15 @@
+package com.ruwel.bongachat.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.ruwel.bongachat.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
