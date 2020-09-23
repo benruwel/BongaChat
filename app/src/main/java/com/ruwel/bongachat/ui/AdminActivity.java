26a4dfd19ace4b43ee48b7604a012f2c2d1d887c
Ben Ruwel <benruwel21@gmail.com>
2020-09-23 19:01:47 +0300

    Admin has ability to get server key from realtime db

--- app/src/main/java/com/ruwel/bongachat/ui/AdminActivity.java
+++ app/src/main/java/com/ruwel/bongachat/ui/AdminActivity.java
@@ -1,15 +1,47 @@
 package com.ruwel.bongachat.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ruwel.bongachat.R;
 
 public class AdminActivity extends AppCompatActivity {
+
+    private String mServerKey;
+    private static final String TAG = "AdminActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        //testing
+        getServerKey();
+    }
+
+    //Now that is the admin activity, we can query db for some-what risky data
+    private void getServerKey() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference.child(getString(R.string.dbnode_server)).orderByValue();
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mServerKey = snapshot.getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
